98f02354e87ca2c2450726357c0bca81e7f56ae7
cleberspirlandeli <contato.cleberrezende@gmail.com>
2019-06-15 14:08:19 -0300

    melhorias no backend
    melhorias no backend para subir a aplicação para o heroku

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 
-mongoose.connect('mongodb+srv://crspirlandeli:92novaSenha!@cluster0-6qsry.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true
 });
 
@@ -24,6 +24,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resiz
 
 app.use(require('./routes'));
 
-server.listen(3333, () => {
-    console.log('Rodando na porta 3333')
-})
+server.listen(process.env.PORT || 3333);
